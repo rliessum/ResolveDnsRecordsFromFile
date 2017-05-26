a540b72ce1a0f3164c672d47931b78e51e098720
Richard van Liessum <richard@liessum.net>
2017-05-26 22:24:20 +0200

    clean up some code

--- resolveDnsRecordsFromFile.py
+++ resolveDnsRecordsFromFile.py
@@ -2,12 +2,11 @@
 import csv
 import os
 import sys
-import socket
-import dns.resolver
 import datetime
-import pprint
+import dns.resolver
 from tabulate import tabulate
 
+FILENAME = "dnsInput.csv"
 
 def main():
     # Run multiple functions under main
@@ -22,87 +21,86 @@ def get_script_path(for_file = None):
 
 
 def read_and_resolve_from_file():
-    fileName = ("/Users/richard/Documents/VisualStudio/WorkDev/resolveWithDNS/dnsInput.csv")    
-    fileNameOut = ("/Users/richard/Documents/VisualStudio/WorkDev/resolveWithDNS/dnsInput-output.csv")
+    fileName = input('Provide a CSV as input [file.csv] ')
+    fileNameOut = ('dnsOutput.csv')
     try:
-        with open((fileName), "r") as fileInput:
+        with open((fileName), 'r') as fileInput:
             fileFound = True
-    except FileNotFoundError:
-        print("Could not locate file " + fileName)
+    except fileFound:
+        print('Could not locate file ' + fileName)
         fileFound = False
     except:
         error = sys.exc_info()
         print(error)
         fileFound = False
     if  fileFound:
-        with open((fileName), "r") as fileInput:
+        with open((fileName), 'r') as fileInput:
             fileFound = True
             allRowsList = csv.reader(fileInput)
             data = list(allRowsList)
             row_count = len(data)
-            print("File used for input is %s" % fileName)
-            print("File used for output is %s" % fileNameOut)
-            print("-" * 50)
-            print("There are %s rows in this file" % row_count)
-            print("-" * 50)
-            print("FileName location is %s:\n" % (get_script_path(fileName)))
-            print("-" * 50)
+            print('File used for input is %s' % fileName)
+            print('File used for output is %s' % fileNameOut)
+            print('-' * 50)
+            print('There are %s rows in this file' % row_count)
+            print('-' * 50)
+            print('FileName location is %s:\n' % (get_script_path(fileName)))
+            print('-' * 50)
 
-    for currentRow in data :
-        for currentWord in currentRow :
-          print(",".join(currentRow))
-          myResolver = dns.resolver.Resolver()
-          myAnswers = myResolver.query((currentWord), "A")
-          for outputRecord in myAnswers:
-              print("%s\n" % (outputRecord))
-          date = datetime.datetime.now().strftime("date-%Y-%m-%d_time-%H-%M-%S")
-          fileNameOut = "dnsoutput"
-          with open((fileNameOut)+'_'+(date)+'.csv', 'a', newline='') as csvfile:
-              csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-              outputRecordList = list()
-              outputRecordList.append(currentRow)
-              print(",".join(currentRow))
-              myResolver = dns.resolver.Resolver()
-              myAnswers = myResolver.query((currentWord), "A")
-          for outputRecord in myAnswers:
-              with open((fileNameOut)+'_'+(date)+'.csv', 'a', newline='') as csvfile:
-                  csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                  print("%s\n" % (outputRecord))
-                  outputRecordList.append(outputRecord)
-                  print ("Updated List with record %s" % (outputRecordList))
-                  csvwriter.writerow(outputRecordList)
+    for currentRow in data:
+        for currentWord in currentRow:
+            (','.join(currentRow))
+            myResolver = dns.resolver.Resolver()
+            myAnswers = myResolver.query((currentWord), 'A')
+            # for outputRecord in myAnswers:
+            #     ('%s\n' % (outputRecord))
+            date = datetime.datetime.now().strftime('date-%Y-%m-%d_time-%H-%M-%S')
+            fileNameOut = 'dnsoutput'
+            with open((fileNameOut)+'_'+(date)+'.csv', 'a', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                outputRecordList = list()
+                outputRecordList.append(currentRow)
+                (','.join(currentRow))
+                myResolver = dns.resolver.Resolver()
+                myAnswers = myResolver.query((currentWord), 'A')
+            for outputRecord in myAnswers:
+                with open((fileNameOut)+'_'+(date)+'.csv', 'a', newline='') as csvfile:
+                    csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    print('%s\n' % (outputRecord))
+                    outputRecordList.append(outputRecord)
+                    print('Updated List with record %s' % (outputRecordList))
+                    csvwriter.writerow(outputRecordList)
 
 
 def read_and_resolve_from_filetable():
-    fileName = input("Provide a CSV as input [file.csv] ")  
-    #fileName = "/Users/richard/Documents/VisualStudio/WorkDev/resolveWithDNS/dnsInput.csv" # for testing, static input
-    with open((fileName),"r") as fileInput :
+    fileName = input('Provide a CSV as input [file.csv] ')
+    with open((fileName),'r') as fileInput :
         allRowsList = csv.reader(fileInput)
         data = list(allRowsList)
         row_count = len(data)
-        print("File used for input is %s\n" % fileName)
-        print("There are %s rows in this file\n" % row_count)
+        print('File used for input is %s\n' % fileName)
+        print('There are %s rows in this file\n' % row_count)
 
     for currentRow in data :
       for currentWord in currentRow :
-           #print(",".join(currentRow))
+           #print(','.join(currentRow))
            myResolver = dns.resolver.Resolver()
-           myAnswers = myResolver.query((currentWord), "A")
+           myAnswers = myResolver.query((currentWord), 'A')
            for outputRecord in myAnswers:
-               #print("%s\n" % (outputRecord))
-                headers = ["Result","IP"]
+                print('%s\n' % (outputRecord))
+                headers = ['Result','IP']
                 table = [(myAnswers),(currentRow)]
-                print(tabulate(table, headers, tablefmt="grid"))
+                print(tabulate(table, headers, tablefmt='grid'))
 
 
-tableOptions = input("Do you the output in tables [yes/no] ").upper()
+tableOptions = input('Do you the output in tables [yes/no]').upper()
 
-if tableOptions == ("YES"):
-    print("Format with tables")
+if tableOptions == ('YES'):
+    print('Format with tables')
     read_and_resolve_from_filetable()
-elif tableOptions == ("NO"):
-    print("Format without tables")
-    print("-" * 50)
+elif tableOptions == ('NO'):
+    print('Format without tables')
+    print('-' * 50)
     read_and_resolve_from_file()
 else:
-    print("No selection has been made!")
+    print('No selection has been made!')
